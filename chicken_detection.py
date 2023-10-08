92807369ff2e00b91c804a24facdb3fe206b90e7
av <antonioverdicchio@gmail.com>
2023-10-08 12:09:54 -0700

    Added train function

--- chicken_detection.py
+++ chicken_detection.py
@@ -3,6 +3,7 @@ from imutils.video import FPS
 
 import tensorflow as tf
 import keras
+from keras import datasets, layers, models
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 import numpy as np
@@ -12,6 +13,42 @@ import time
 import cv2
 
 import matplotlib.pyplot as plt
+
+# Trains a CNN on CIFAR10
+def train_network(save_name, num_epochs):
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+    # Normalize pixel values to be between 0 and 1
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+
+    # Creating the a sequential network architecture
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+
+    model.compile(optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
+
+    history = model.fit(train_images, train_labels, epochs=num_epochs, 
+                    validation_data=(test_images, test_labels))
+    
+    model.save(save_name)
+
+
+
+"""
+-------------------------------------------------------------------------------------------------------------------------
+Running video stream and inference
+-------------------------------------------------------------------------------------------------------------------------
+
+"""
 
 loaded_model = keras.models.load_model("model20.keras")
 
