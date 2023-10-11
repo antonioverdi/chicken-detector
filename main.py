287c28703e82f9efa031866f2ddde9010d00c289
av <antonioverdicchio@gmail.com>
2023-10-11 09:32:49 -0700

    removed cap resizing in main.py

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from PIL import Image
 torch.backends.quantized.engine = 'qnnpack'
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
-cap.set(cv2.CAP_PROP_FPS, 36)
 
 preprocess = transforms.Compose([
     transforms.ToTensor(),
