dd5ff5d984e7c559f22a583fa9b9c798cc0a6465
av <antonioverdicchio@gmail.com>
2023-10-10 18:03:20 -0700

    Still trying to fix imports

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-net = models.quantization.mobilenet_v2(weights=MobileNet_V2_QuantizedWeights.IMAGENET1K_QNNPACK_V1)
+net = models.quantization.mobilenet_v2(weights=MobileNet_V2_QuantizedWeights.IMAGENET1K_QNNPACK_V1, quantize=True)
 # jit model to take it from ~20fps to ~30fps
 net = torch.jit.script(net)
 
