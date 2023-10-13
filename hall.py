e27ab615586062e7a9bb4625c2da492cf05e5928
av <antonioverdicchio@gmail.com>
2023-10-13 13:39:31 -0700

    Working out sensor and motor interface

--- hall.py
+++ hall.py
@@ -14,34 +14,25 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
-GPIO.setup(hall1,GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-GPIO.setup(hall2,GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(hall1,GPIO.IN) 
+GPIO.setup(hall2,GPIO.IN) 
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
 p=GPIO.PWM(en,1000)
 p.start(25)
 
 
-#This function will be called if a change is detected
-def change_detected(channel):
-    if GPIO.input(hall1) == GPIO.LOW:
-        print('Magnetic material detected')
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-    else:
-        print('No magnetic material')
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-
-# Register event-listener on falling and raising
-# edge on HALL-sensor input. Call "change_detected" as
-# callback
-GPIO.add_event_detect(hall1, GPIO.BOTH, change_detected, bouncetime=25)
-
 # The main-loop does nothing. All is done by the event-listener
 try:
     while True:
-        pass
+        if(GPIO.input(hall1) == False):
+            GPIO.output(in1,GPIO.HIGH)
+            GPIO.output(in2,GPIO.LOW)
+            print("magnet detected")
+        else:
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.LOW)
+            print("magnetic field not detected")
 
 # Quit on Ctrl-c
 except KeyboardInterrupt:
