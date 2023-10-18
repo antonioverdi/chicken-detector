a2f3d6fba990d677dc8f79eff00355183deea8c8
av <antonioverdicchio@gmail.com>
2023-10-18 14:41:07 -0700

    Hall sensors work, adding chicken detection

--- hall.py
+++ hall.py
@@ -96,30 +96,71 @@ def safe_kill():
 
 
 # DOOR OPERATING CODE
+def run_door():
+    BottomHall=GPIO.input(5)
+    TopHall=GPIO.input(6)
+    if BottomHall==0:print('Door is locked')
+    if TopHall==0:print('Door is open')
+    if BottomHall==0: #Door is locked
+            print('The door is locked!')
+            print('The door is going up!')
+            while TopHall==1:
+                    door_up()
+                    TopHall=GPIO.input(6)
+            if TopHall==0:
+                    print('Door is open!')
+                    safe_kill()
+    elif TopHall==0: #Door is open
+            print('The door is open!')
+            print('The door is going down!')
+            while BottomHall==1:
+                    door_down()
+                    BottomHall=GPIO.input(5)
+            if BottomHall==0:
+                    print('Door is locked!')
+                    safe_kill()
 
-BottomHall=GPIO.input(5)
-TopHall=GPIO.input(6)
-if BottomHall==0:print('Door is locked')
-if TopHall==0:print('Door is open')
-if BottomHall==0: #Door is locked
-		print('The door is locked!')
-		print('The door is going up!')
-		while TopHall==1:
-				door_up()
-				TopHall=GPIO.input(6)
-		if TopHall==0:
-				print('Door is open!')
-				safe_kill()
-elif TopHall==0: #Door is open
-		print('The door is open!')
-		print('The door is going down!')
-		while BottomHall==1:
-				door_down()
-				BottomHall=GPIO.input(5)
-		if BottomHall==0:
-				print('Door is locked!')
-				safe_kill()
 
+with torch.no_grad():
+    while True:
+        # Read frame in from webcam
+        ret, image = cam.read()
+
+        # Process the image for mobilenet
+        input_tensor = preprocess(image)
+        input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+        # move the input and model to GPU for speed if available
+        if torch.cuda.is_available():
+            input_batch = input_batch.to('cuda')
+            model.to('cuda')
+
+        output = model(input_batch)
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+
+        # Show top categories per image
+        top5_prob, top5_catid = torch.topk(probabilities, 5)
+        detected = {}
+        for i in range(top5_prob.size(0)):
+            detected.add(categories[top5_catid[i]])
+        
+        if len(detected.intersection(targets)) > 0:
+            print("Chicken Detected")
+            run_door()
+
+        # Display the resulting frame
+        cv2.imshow('Video Test', image)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        # Press 'q' key to break the loop
+        if key == ord("q"):
+            break
+
+    # When everything done, release the capture
+    cam.release()
+    cv2.destroyAllWindows()
 
 safe_kill()
 
