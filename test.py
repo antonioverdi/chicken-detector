9e5b1d72c2bab0269d4e05db1ab579b576557d6a
av <antonioverdicchio@gmail.com>
2023-10-20 15:36:26 -0700

    replacing hall sensors with reed switches'

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ bottomreed=6
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(topreed, GPIO.IN) 
-GPIO.setup(bottomreed, GPIO.IN) 
+GPIO.setup(topreed, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(bottomreed, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 
-while True:
-    if GPIO.input(5):
-        print("topreed 1")
-    else:
-        print("topreed 0")
 
-    if GPIO.input(6):
-        print("bottomreed 1")
-    else:
-        print("bottomread 0")
+if GPIO.input(5):
+    print("topreed 1")
+else:
+    print("topreed 0")
+
+if GPIO.input(6):
+    print("bottomreed 1")
+else:
+    print("bottomread 0")
